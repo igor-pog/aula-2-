15da1d15df4f521c3a9eca9e6f94023066f40051
igor-pog <igordelemosramos@gmail.com>
2025-05-27 20:20:17 -0300

    tree commit

---
+++ ex2.php
@@ -0,0 +1,10 @@
+<?php
+$val1	= 5 ;
+$val2	= 9 ;
+$val3	= 3 ;
+
+$valm = ($val1 + $val2 + $val3)/3;
+
+echo " Sua Media é: $valm "
+
+?>
---
+++ ex3.php
@@ -0,0 +1,6 @@
+<?php
+$val = 5; 
+$por = $val * 0.15;
+echo "resultado = $por"; 
+
+?>
